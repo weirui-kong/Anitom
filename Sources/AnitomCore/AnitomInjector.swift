066cbc44d427b6471eb8eaf1f17cfd3d6d79019b
孔维锐 <weiruik@outlook.com>
2023-11-26 00:47:56 +0800

    Add onHover option and customizable animatation closure. Remove redundant codes.

--- Sources/AnitomCore/AnitomInjector.swift
+++ Sources/AnitomCore/AnitomInjector.swift
@@ -6,11 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
 class AnitomInjector: ObservableObject{
-    @Published var isOn: Bool = false
-    @Published var isShaddowOn = false
+    @Published var isRecursiveOn: Bool = false
+    @Published var isShaddowOn: Bool = false
     @Published var offsetX: CGFloat = 0
     @Published var offsetY: CGFloat = 0
     @Published var scale: CGFloat = 1
+    @Published var animation: (TimeInterval) -> Animation = { t in
+        Animation.spring(duration: t)
+    }
+    
+    init(isRecursiveOn: Bool = false, isShaddowOn: Bool = false, offsetX: CGFloat = 0, offsetY: CGFloat = 0, scale: CGFloat = 1, animation: @escaping (TimeInterval) -> Animation = { t in Animation.spring(duration: t)}) {
+        self.isRecursiveOn = isRecursiveOn
+        self.isShaddowOn = isShaddowOn
+        self.offsetX = offsetX
+        self.offsetY = offsetY
+        self.scale = scale
+        self.animation = animation
+    }
 }
-
